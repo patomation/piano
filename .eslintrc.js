f2eb319637d620fc66056df6071299a77df7d991
patomation <patomation@gmail.com>
2020-02-25 15:06:03 +0700

    style: proper eslint for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,50 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  overrides: [
+    {
+      /* Turns off typescript rules for js files. ie webpack.config.js and node scripts */
+      files: ['*.js'],
+      parser: 'espree',
+      extends: [
+        'standard'
+      ],
+      rules: {
+        '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/no-empty-function': 0
+      },
+    }
+  ],
+  plugins: [
+    'react',
+    'jest',
+    '@typescript-eslint'
+  ],
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
+    jest: true
   },
-  extends: [
-    'standard',
-    'plugin:react/recommended'
-  ],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
-  plugins: [
-    'react', 'jest'
-  ],
   rules: {
+    '@typescript-eslint/member-delimiter-style': 0,
+    'react/prop-types': 0 // I will auto generate prop types
   }
 }
